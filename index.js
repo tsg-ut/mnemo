e8eec1c82ce4323ec8006eb92cb1a24d646c70c2
Travis CI <hakatasiloving@gmail.com>
2016-07-02 11:24:54 +0000

    Update build

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ var Data = function Data(board, value) {
 	this.value = value;
 	this.$element = $('<div/>', { 'class': 'data', text: value });
 	this.board.$board.append(this.$element);
+	this.currentBlock = this.board.inputBlock;
 };
 
 module.exports = Data;
