ba419f5bb4aab8cbbc4cc12c30b4148a4cf74c7e
Koki Takahashi <hakatasiloving@gmail.com>
2016-08-06 16:14:54 +0900

    Move Travis scripts into bin directory

---
+++ bin/after_success.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit on error
+set -ev
+
+if [ "$TRAVIS_SECURE_ENV_VARS" != "true" ] || [ "$TRAVIS_NODE_VERSION" != "node" ] || [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+	exit 0
+fi
+
+git checkout f2b3222e3e930b33c5618b26effcf4be451841ed .gitignore
+git status
+git add --all
+git commit -m "Update build"
+git push "https://${GH_TOKEN}@github.com/tsg-ut/mnemo.git" gh-pages:gh-pages --follow-tags > /dev/null 2>&1
---
+++ bin/before_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit on error
+set -ev
+
+if [ "$TRAVIS_SECURE_ENV_VARS" != "true" ] || [ "$TRAVIS_NODE_VERSION" != "node" ] || [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+	exit 0
+fi
+
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+git fetch
+git config user.name "Travis CI"
+git config user.email "hakatasiloving@gmail.com"
+git checkout gh-pages
+git checkout master .
