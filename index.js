a435d1b9927079d2844c101a007129400ac0ee3a
Travis CI <info@tsg.ne.jp>
2016-09-15 06:31:29 +0000

    Update build - 5f7d7191bca1a61964bc65cd7518cf09ca07a474

--- index.js
+++ index.js
@@ -1244,9 +1244,17 @@ require('./analytics');
 require('core-js/es5');
 require('core-js/es6');
 
+var querystring = require('querystring');
 var $ = require('jquery');
 var stageConfigs = require('./stages.json');
 var Game = require('./game');
+
+// parse GET parameters of the location
+var params = querystring.parse(location.search.slice(1));
+
+if (params.fx === 'off') {
+	$.fx.off = true;
+}
 
 $(document).ready(function () {
 	new Game(stageConfigs);
@@ -1259,7 +1267,7 @@ $(document).ready(function () {
 	});
 });
 
-},{"./analytics":1,"./game":7,"./stages.json":11,"core-js/es5":15,"core-js/es6":16,"jquery":264}],9:[function(require,module,exports){
+},{"./analytics":1,"./game":7,"./stages.json":11,"core-js/es5":15,"core-js/es6":16,"jquery":264,"querystring":268}],9:[function(require,module,exports){
 'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -3314,7 +3322,7 @@ var objectKeys = Object.keys || function (obj) {
   return keys;
 };
 
-},{"util/":268}],15:[function(require,module,exports){
+},{"util/":271}],15:[function(require,module,exports){
 require('../modules/es6.object.create');
 require('../modules/es6.object.define-property');
 require('../modules/es6.object.define-properties');
@@ -19115,6 +19123,185 @@ process.chdir = function (dir) {
 process.umask = function() { return 0; };
 
 },{}],266:[function(require,module,exports){
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+
+// If obj.hasOwnProperty has been overridden, then calling
+// obj.hasOwnProperty(prop) will break.
+// See: https://github.com/joyent/node/issues/1707
+function hasOwnProperty(obj, prop) {
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
+
+module.exports = function(qs, sep, eq, options) {
+  sep = sep || '&';
+  eq = eq || '=';
+  var obj = {};
+
+  if (typeof qs !== 'string' || qs.length === 0) {
+    return obj;
+  }
+
+  var regexp = /\+/g;
+  qs = qs.split(sep);
+
+  var maxKeys = 1000;
+  if (options && typeof options.maxKeys === 'number') {
+    maxKeys = options.maxKeys;
+  }
+
+  var len = qs.length;
+  // maxKeys <= 0 means that we should not limit keys count
+  if (maxKeys > 0 && len > maxKeys) {
+    len = maxKeys;
+  }
+
+  for (var i = 0; i < len; ++i) {
+    var x = qs[i].replace(regexp, '%20'),
+        idx = x.indexOf(eq),
+        kstr, vstr, k, v;
+
+    if (idx >= 0) {
+      kstr = x.substr(0, idx);
+      vstr = x.substr(idx + 1);
+    } else {
+      kstr = x;
+      vstr = '';
+    }
+
+    k = decodeURIComponent(kstr);
+    v = decodeURIComponent(vstr);
+
+    if (!hasOwnProperty(obj, k)) {
+      obj[k] = v;
+    } else if (isArray(obj[k])) {
+      obj[k].push(v);
+    } else {
+      obj[k] = [obj[k], v];
+    }
+  }
+
+  return obj;
+};
+
+var isArray = Array.isArray || function (xs) {
+  return Object.prototype.toString.call(xs) === '[object Array]';
+};
+
+},{}],267:[function(require,module,exports){
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+
+var stringifyPrimitive = function(v) {
+  switch (typeof v) {
+    case 'string':
+      return v;
+
+    case 'boolean':
+      return v ? 'true' : 'false';
+
+    case 'number':
+      return isFinite(v) ? v : '';
+
+    default:
+      return '';
+  }
+};
+
+module.exports = function(obj, sep, eq, name) {
+  sep = sep || '&';
+  eq = eq || '=';
+  if (obj === null) {
+    obj = undefined;
+  }
+
+  if (typeof obj === 'object') {
+    return map(objectKeys(obj), function(k) {
+      var ks = encodeURIComponent(stringifyPrimitive(k)) + eq;
+      if (isArray(obj[k])) {
+        return map(obj[k], function(v) {
+          return ks + encodeURIComponent(stringifyPrimitive(v));
+        }).join(sep);
+      } else {
+        return ks + encodeURIComponent(stringifyPrimitive(obj[k]));
+      }
+    }).join(sep);
+
+  }
+
+  if (!name) return '';
+  return encodeURIComponent(stringifyPrimitive(name)) + eq +
+         encodeURIComponent(stringifyPrimitive(obj));
+};
+
+var isArray = Array.isArray || function (xs) {
+  return Object.prototype.toString.call(xs) === '[object Array]';
+};
+
+function map (xs, f) {
+  if (xs.map) return xs.map(f);
+  var res = [];
+  for (var i = 0; i < xs.length; i++) {
+    res.push(f(xs[i], i));
+  }
+  return res;
+}
+
+var objectKeys = Object.keys || function (obj) {
+  var res = [];
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) res.push(key);
+  }
+  return res;
+};
+
+},{}],268:[function(require,module,exports){
+'use strict';
+
+exports.decode = exports.parse = require('./decode');
+exports.encode = exports.stringify = require('./encode');
+
+},{"./decode":266,"./encode":267}],269:[function(require,module,exports){
 if (typeof Object.create === 'function') {
   // implementation from standard node.js 'util' module
   module.exports = function inherits(ctor, superCtor) {
@@ -19139,14 +19326,14 @@ if (typeof Object.create === 'function') {
   }
 }
 
-},{}],267:[function(require,module,exports){
+},{}],270:[function(require,module,exports){
 module.exports = function isBuffer(arg) {
   return arg && typeof arg === 'object'
     && typeof arg.copy === 'function'
     && typeof arg.fill === 'function'
     && typeof arg.readUInt8 === 'function';
 }
-},{}],268:[function(require,module,exports){
+},{}],271:[function(require,module,exports){
 (function (process,global){
 // Copyright Joyent, Inc. and other Node contributors.
 //
@@ -19737,5 +19924,5 @@ function hasOwnProperty(obj, prop) {
 
 }).call(this,require('_process'),typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 
-},{"./support/isBuffer":267,"_process":265,"inherits":266}]},{},[8])
+},{"./support/isBuffer":270,"_process":265,"inherits":269}]},{},[8])
 //# sourceMappingURL=index.js.map
---
+++ test/functional/game.ls
@@ -0,0 +1,56 @@
+require! {
+  chai: {expect}
+}
+
+It = global.it
+
+describe 'Game Screen' ->
+  describe 'Stage 1' ->
+    before ->
+      @nightmare
+      .refresh!
+      .wait -> document.readyState is 'complete'
+      .click 'button.select'
+      .click '.stage-name:nth-child(1)'
+      .click '.modal .close'
+
+    It 'navigates to game screen' ->
+      @nightmare
+      .evaluate -> document.query-selector '.screens' .classList.contains 'gaming'
+      .then -> expect it .to.be.true
+
+    It 'can place blocks' ->
+      @nightmare
+      .click '.panel > .block[data-type=wireI]'
+      .click '.board .block[data-x="1"][data-y="0"]'
+      .then ~>
+        @nightmare
+        .evaluate ->
+          document.query-selector '.board .block[data-x="1"][data-y="0"]'
+          .get-attribute 'data-type'
+      .then ~>
+        expect it .to.equal 'wireI'
+
+        @nightmare
+        .evaluate ->
+          document.query-selector '.board .block[data-x="1"][data-y="0"]'
+          .get-attribute 'data-rotate'
+      .then ->
+        expect it .to.equal '0'
+
+    It 'fails when executed' ->
+      @nightmare
+      .click 'button.execute'
+      .wait -> document.query-selector-all '.data' .length is 0
+      .evaluate -> document.query-selector '.statement' .text-content
+      .then -> expect it .to.equal '入り口と出口をつなげてみよう!'
+
+    It 'succeeds when placed more blocks correctly' ->
+      @nightmare
+      .click '.panel > .block[data-type=wireI]'
+      .click '.board .block[data-x="1"][data-y="1"]'
+      .click '.board .block[data-x="1"][data-y="2"]'
+      .click 'button.execute'
+      .wait -> document.query-selector-all '.data' .length is 0
+      .evaluate -> document.query-selector '.statement' .text-content
+      .then -> expect it .to.equal '数を2倍してみよう!'
