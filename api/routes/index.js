86be489755f463e43c08ca96768867713acd781e
Koki Takahashi <hakatasiloving@gmail.com>
2016-09-15 17:18:26 +0900

    Getting started with sequelize

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mnemo', '', '', {
+	dialect: 'sqlite',
+	storage: 'db.sqlite3',
+});
+
+const Submission = sequelize.define('submission', {
+	userName: Sequelize.STRING,
+	point: Sequelize.INTEGER,
+});
+
 router.get('/', (req, res) => {
 	res.end('hoge');
 });
