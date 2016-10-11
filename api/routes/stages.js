320611d625aa96421f3af09a61ae2d8f5e516b42
Koki Takahashi <hakatasiloving@gmail.com>
2016-10-11 12:32:45 +0900

    Add /stages/:stage endpoint

--- api/routes/stages.js
+++ api/routes/stages.js
@@ -9,4 +9,23 @@ router.get('/', (req, res) => {
 	});
 });
 
+router.get('/:stage', (req, res) => {
+	const stageName = req.params.stage;
+
+	Stages.findOne({
+		where: {
+			name: stageName,
+		},
+	}).then((stage) => {
+		if (stage === null) {
+			res.status(404).json({
+				error: true,
+				message: 'not found',
+			});
+		} else {
+			res.json(stage);
+		}
+	});
+});
+
 module.exports = router;
