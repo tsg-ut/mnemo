c4c449221ba8f834c5b7d4c98ec1734710a9cd6f
Koki Takahashi <hakatasiloving@gmail.com>
2016-10-11 09:38:11 +0900

    Fix seeder

--- api/index.js
+++ api/index.js
@@ -6,5 +6,6 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/', require('./routes/index'));
+app.use('/stages', require('./routes/stages'));
 
 app.listen(3000);
--- api/seeders/20161003085039-first-seed.js
+++ api/seeders/20161003085039-first-seed.js
@@ -1,26 +1,30 @@
 const stages = require('../../lib/stages.json');
+const Stages = require('../models/stage');
 
 module.exports = {
-	up: (queryInterface) => Promise.all([
+	up: (queryInterface) => (
 		queryInterface.bulkInsert('stages', stages.map((stage, index) => ({
 			name: stage.name || `stage${index}`,
 			createdAt: new Date(),
 			updatedAt: new Date(),
-		})), {}),
-		queryInterface.bulkInsert('submissions', [{
-			name: 'hakatashi',
-			board: JSON.stringify([{
-				x: 0,
-				y: 0,
-				name: 'wireI',
-				rotate: 0,
-			}]),
-			score: 100,
-			stageId: 1,
-			createdAt: new Date(),
-			updatedAt: new Date(),
-		}]),
-	]),
+		})), {}).then(() => (
+			Stages.findOne({where: {name: 'wire01'}})
+		)).then((stage) => (
+			queryInterface.bulkInsert('submissions', [{
+				name: 'hakatashi',
+				board: JSON.stringify([{
+					x: 0,
+					y: 0,
+					name: 'wireI',
+					rotate: 0,
+				}]),
+				score: 100,
+				stageId: stage.id,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			}])
+		))
+	),
 
 	down: (queryInterface) => (
 		queryInterface.bulkDelete('submissions', null, {}).then(
