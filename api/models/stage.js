573e5ffe7530a4416107cd61edb3f8b7524c293e
Koki Takahashi <hakatasiloving@gmail.com>
2016-10-11 10:05:29 +0900

    Implement /stages endpoint

--- api/models/stage.js
+++ api/models/stage.js
@@ -1,5 +1,8 @@
+const Sequelize = require('sequelize');
 const sequelize = require('./');
 
-const Stage = sequelize.define('stages');
+const Stage = sequelize.define('stages', {
+	name: Sequelize.TEXT,
+});
 
 module.exports = Stage;
--- api/routes/stages.js
+++ api/routes/stages.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const Stages = require('../models/stage');
 
 router.get('/', (req, res) => {
-	Stages.findOne({where: {name: 'wire01'}}).then((stage) => {
-		res.end(stage.createdAt.toString());
+	Stages.findAll().then((stages) => {
+		res.json(stages);
 	});
 });
 
