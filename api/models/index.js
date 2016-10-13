a12af788bfadb1f8afe3da1ea827d58bf5ed9575
Travis CI <info@tsg.ne.jp>
2016-10-13 03:15:15 +0000

    Update build - 9877ffab31fa8918f318c0394855d93070625abc

---
+++ api/models/index.js
@@ -0,0 +1,7 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+module.exports = sequelize;
