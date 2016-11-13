adfaf8e4c17a1683eeed9c5c711b8d6bd4a7c116
Koki Takahashi <hakatasiloving@gmail.com>
2016-11-13 17:35:45 +0900

    Dynamically bind app to the env port

--- api/index.js
+++ api/index.js
@@ -8,6 +8,6 @@ app.use(bodyParser.json());
 app.use('/', require('./routes/index'));
 app.use('/stages', require('./routes/stages'));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
