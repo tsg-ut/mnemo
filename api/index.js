04dfeb1d11abd799a11f8a4bd8072668e82c8fcb
Koki Takahashi <hakatasiloving@gmail.com>
2016-11-14 09:18:29 +0900

    Enforce HTTPS in production mode

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const enforce = require('express-sslify');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(enforce.HTTPS({trustProtoHeader: true}));
+}
 
 app.use('/', require('./routes/index'));
 app.use('/stages', require('./routes/stages'));
