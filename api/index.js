f0cbc61ae2333f2d4c98497b8aad3e83c99c1661
Koki Takahashi <hakatasiloving@gmail.com>
2016-11-14 09:39:45 +0900

    Remove express-sslify and use forcedomain module

--- api/index.js
+++ api/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const enforce = require('express-sslify');
+const forceDomain = require('forcedomain');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(enforce.HTTPS({trustProtoHeader: true}));
-	app.use((req, res, done) => {
-		console.log(req.hostname);
-		done();
-	});
+	app.use(forceDomain({
+		hostname: 'api.mnemo.pro',
+		protocol: 'https',
+	}));
 }
 
 app.use('/', require('./routes/index'));
