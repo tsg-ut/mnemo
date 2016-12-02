3962ba2bad9fc34fb605095e3420b45fa1b39fc4
Koki Takahashi <hakatasiloving@gmail.com>
2016-12-03 04:15:04 +0900

    Remove seeds of test data

--- api/seeders/20161003085039-first-seed.js
+++ api/seeders/20161003085039-first-seed.js
@@ -1,5 +1,4 @@
 const stages = require('../../stages');
-const Stages = require('../models/stage');
 
 module.exports = {
 	up: (queryInterface) => (
@@ -7,68 +6,7 @@ module.exports = {
 			name: stage.name || `stage${index}`,
 			createdAt: new Date(),
 			updatedAt: new Date(),
-		})), {}).then(() => (
-			Stages.findOne({where: {name: 'wire01'}})
-		)).then((stage) => {
-			if (process.env.NODE_ENV !== 'production') {
-				const board = JSON.stringify([{
-					x: 1,
-					y: 0,
-					name: 'wireI',
-					rotate: 0,
-				}, {
-					x: 1,
-					y: 1,
-					name: 'wireI',
-					rotate: 0,
-				}, {
-					x: 1,
-					y: 2,
-					name: 'wireI',
-					rotate: 0,
-				}]);
-
-				return queryInterface.bulkInsert('submissions', [{
-					name: 'kurgm',
-					board,
-					score: 9000,
-					blocks: 3,
-					clocks: 3,
-					stageId: stage.id,
-					createdAt: new Date(Date.now() - 24 * 60 * 60 * 1000),
-					updatedAt: new Date(Date.now() - 24 * 60 * 60 * 1000),
-				}, {
-					name: 'moratorium08',
-					board,
-					score: 10000,
-					blocks: 3,
-					clocks: 3,
-					stageId: stage.id,
-					createdAt: new Date(),
-					updatedAt: new Date(),
-				}, {
-					name: 'Yosshi999',
-					board,
-					score: 9000,
-					blocks: 3,
-					clocks: 3,
-					stageId: stage.id,
-					createdAt: new Date(),
-					updatedAt: new Date(),
-				}, {
-					name: 'hakatashi',
-					board,
-					score: 10000,
-					blocks: 3,
-					clocks: 3,
-					stageId: stage.id,
-					createdAt: new Date(Date.now() - 24 * 60 * 60 * 1000),
-					updatedAt: new Date(Date.now() - 24 * 60 * 60 * 1000),
-				}]);
-			}
-
-			return Promise.resolve();
-		})
+		})), {})
 	),
 
 	down: (queryInterface) => (
