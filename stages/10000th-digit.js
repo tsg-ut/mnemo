1b65c80ad990ecae21ec6afde22dbe955122ce2a
Koki Takahashi <hakatasiloving@gmail.com>
2017-06-03 19:44:23 +0900

    Kill pseudo-solutions of 10000th-digit stage

--- stages/10000th-digit.js
+++ stages/10000th-digit.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: '10000th-digit',
-	version: 1,
+	version: 2,
 	parts: {
 		wireI: null,
 		wireL: null,
@@ -45,31 +45,96 @@ module.exports = {
 	},
 	inputX: 6,
 	outputX: 6,
-	input: [null, null, null, 61],
-	output: [null, null, null, 3],
+	input: [null, null, null, null, 107],
+	output: [null, null, null, null, 3],
 	ioGenerator: (random) => {
+		const iterations = (n) => (
+			10000 % (n - 1) === 0 ? n - 1 : 10000 % (n - 1)
+		);
+
 		const calc = (p) => {
 			let reminder = 1;
-			for (let i = 0; i < 10000 - 1; i++) {
+			for (let i = 0; i < iterations(p) - 1; i++) {
 				reminder = (reminder * 10) % p;
 			}
 			return Math.floor(reminder * 10 / p);
 		};
 
-		const candidates = [7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59];
+		const candidates = [
+			7,
+			11,
+			13,
+			17,
+			19,
+			23,
+			29,
+			31,
+			37,
+			41,
+			43,
+			47,
+			53,
+			59,
+			61,
+			67,
+			71,
+			73,
+			79,
+			83,
+			89,
+			97,
+			101,
+			103,
+			// 107
+			109,
+			113,
+			127,
+			131,
+			137,
+			139,
+			149,
+			151,
+			157,
+			163,
+			167,
+			173,
+			179,
+		].filter((n) => iterations(n) <= 36);
+
 		const inputs = [];
 
-		// Shuffle array and take heading 3
-		const index1 = Math.floor(random() * 14);
-		inputs.push(candidates[index1]);
-		candidates[index1] = candidates[0];
+		// Take input which takes at least 24 iterations
+		const heavyInputs = candidates.filter((n) => iterations(n) >= 24 && calc(n) !== 3);
+		inputs.push(heavyInputs[Math.floor(random() * heavyInputs.length)]);
 
-		const index2 = Math.floor(random() * 13) + 1;
-		inputs.push(candidates[index2]);
-		candidates[index2] = candidates[1];
+		// Generate outputMap
+		const outputMap = new Map();
+		candidates.forEach((n) => {
+			if (!outputMap.has(calc(n))) {
+				outputMap.set(calc(n), []);
+			}
+			outputMap.get(calc(n)).push(n);
+		});
 
-		const index3 = Math.floor(random() * 12) + 2;
-		inputs.push(candidates[index3]);
+		const outputCandidates = Array.from(outputMap.keys()).filter((n) => (
+			n !== 3 && n !== calc(inputs[0])
+		));
+
+		// Take unique outputs by 3 and get corresponding inputs
+
+		const index1 = Math.floor(random() * outputCandidates.length);
+		const inputCandidates1 = outputMap.get(outputCandidates[index1]);
+		inputs.push(inputCandidates1[Math.floor(random() * inputCandidates1.length)]);
+		outputCandidates[index1] = outputCandidates[0];
+
+		const index2 = Math.floor(random() * (outputCandidates.length - 1)) + 1;
+		const inputCandidates2 = outputMap.get(outputCandidates[index2]);
+		inputs.push(inputCandidates2[Math.floor(random() * inputCandidates2.length)]);
+		outputCandidates[index2] = outputCandidates[1];
+
+		const index3 = Math.floor(random() * (outputCandidates.length - 2)) + 2;
+		const inputCandidates3 = outputMap.get(outputCandidates[index3]);
+		inputs.push(inputCandidates3[Math.floor(random() * inputCandidates3.length)]);
 
 		inputs.sort((a, b) => a - b);
 
@@ -78,12 +143,14 @@ module.exports = {
 				inputs[0],
 				inputs[1],
 				inputs[2],
-				61,
+				inputs[3],
+				107,
 			],
 			output: [
 				calc(inputs[0]),
 				calc(inputs[1]),
 				calc(inputs[2]),
+				calc(inputs[3]),
 				3,
 			],
 		};
