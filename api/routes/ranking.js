a97ca1d911b9de4291ae75e2ddfdf4adb234a90c
Koki Takahashi <hakatasiloving@gmail.com>
2017-08-07 22:51:59 +0900

    Implement ranking

--- api/routes/ranking.js
+++ api/routes/ranking.js
@@ -3,10 +3,33 @@ const router = Router();
 
 const Stages = require('../models/stage');
 const Submissions = require('../models/submission');
+const sequelize = require('../models');
 
 router.get('/', async (req, res) => {
-	const submissions = await Submissions.findAll();
-	res.json(submissions);
+	const submissions = await Submissions.findAll({
+		attributes: [
+			'name',
+			[sequelize.fn('sum', sequelize.col('score')), 'totalScore'],
+		],
+		include: [{
+			model: Stages,
+			where: {
+				migratedVersion: {
+					$col: 'submissions.version',
+				},
+			},
+		}],
+		group: ['submissions.name'],
+		order: [
+			[sequelize.fn('sum', sequelize.col('score')), 'DESC'],
+		],
+		limit: 20,
+	});
+
+	res.json(submissions.map((submission) => ({
+		name: submission.name,
+		score: submission.dataValues.totalScore,
+	})));
 });
 
 module.exports = router;
--- api/test/routes/xranking.js
+++ api/test/routes/xranking.js
@@ -42,34 +42,67 @@ describe('/ranking', () => {
 
 	describe('GET /ranking', () => {
 		it('retuns JSON of the submission array', async () => {
-			const stage = await Stages.create({
+			const stage1 = await Stages.create({
 				name: 'wire01',
 				migratedVersion: 1,
 			}, {transaction});
 
-			await Submissions.bulkCreate([{
-				name: 'hakatashi',
+			const stage2 = await Stages.create({
+				name: 'calc01',
+				migratedVersion: 1,
+			}, {transaction});
+
+			const submissionFactory = {
+				name: 'A',
 				board: JSON.stringify([]),
 				blocks: 0,
 				clocks: 0,
 				score: 0,
-				stage,
 				version: 1,
-			}], {transaction});
+			};
+
+			await Submissions.bulkCreate([
+				Object.assign({}, submissionFactory, {
+					stageId: stage1.id,
+					name: 'A',
+					score: 10000,
+				}),
+				Object.assign({}, submissionFactory, {
+					stageId: stage1.id,
+					name: 'B',
+					score: 10000,
+				}),
+				Object.assign({}, submissionFactory, {
+					stageId: stage1.id,
+					name: 'C',
+					score: 10000,
+				}),
+				Object.assign({}, submissionFactory, {
+					stageId: stage2.id,
+					name: 'A',
+					score: 10000,
+				}),
+				Object.assign({}, submissionFactory, {
+					stageId: stage2.id,
+					name: 'B',
+					score: 5000,
+				}),
+			], {transaction});
 
 			const res = await chai.request(app).get('/ranking');
 
 			expect(res).to.have.status(200);
 			expect(res).to.be.json;
-			expect(res.body).to.be.an('array');
-			expect(res.body).to.have.length(1);
-
-			res.body.forEach((submission) => {
-				expect(submission).to.have.property('name');
-				expect(submission).to.have.property('blocks');
-				expect(submission).to.have.property('clocks');
-				expect(submission).to.have.property('score');
-			});
+			expect(res.body).to.deep.equal([{
+				name: 'A',
+				score: 20000,
+			}, {
+				name: 'B',
+				score: 15000,
+			}, {
+				name: 'C',
+				score: 10000,
+			}]);
 		});
 	});
 });
