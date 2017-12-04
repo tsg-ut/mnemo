99954b85611c3e56c744b1ce889d1543486be907
cookie-s <cookie-s@users.noreply.github.com>
2017-12-05 03:42:14 +0900

    setState updater

--- lib/block-component.jsx
+++ lib/block-component.jsx
@@ -42,103 +42,128 @@ class BlockComponent extends React.Component {
 		};
 
 		this.props.block.on('get', ({direction, data}) => {
-			this.setState({
-				inputData: this.state.inputData.set(data, {
+			this.setState( (prevState, props) => ({
+				inputData: prevState.inputData.set(data, {
 					direction,
-					isErasing: this.props.status === 'stop',
+					isErasing: props.status === 'stop',
 				}),
-			});
+			}));
+		});
+
+		this.props.block.on('reject', (data) => {
+				this.setState( (prevState) => {
+					let {inputData, animatingData, outputData} = prevState;
+
+					if (inputData.has(data)) {
+						const datum = inputData.get(data);
+						inputData = inputData.set(data, Object.assign(datum, {isErasing: true}));
+					};
+					if (animatingData.has(data)) {
+						const datum = animatingData.get(data);
+						animatingData = animatingData.set(data, Object.assign(datum, {isErasing: true}));
+					}
+					if (outputData.has(data)) {
+						const datum = outputData.get(data);
+						outputData = outputData.set(data, Object.assign(datum, {isErasing: true}));
+					}
+					return {inputData, animatingData, outputData};
+				});
 		});
 
 		this.props.block.on('pass', async (passEvent) => {
 			const inputAnimations = [];
+			const inwardPromise = new Promise( (resolve) => {
+				this.setState(
+					// updater
+					(prevState) => {
+						let {inputData, animatingData} = prevState;
 
-			for (const [direction, data] of passEvent.in.entries()) {
-				this.setState({
-					animatingData: this.state.animatingData.set(data, {
-						direction,
-						isInward: true,
-						isErasing: this.props.status === 'stop',
-					}),
-					inputData: this.state.inputData.delete(data),
-				});
+						for (const [direction, data] of passEvent.in.entries()) {
+							animatingData = animatingData.set(data, {
+								direction,
+								isInward: true,
+								isErasing: this.props.status === 'stop',
+							});
+							inputData = inputData.delete(data);
 
-				inputAnimations.push(new Promise((resolve) => {
-					this.dataAnimationResolvers.set(data, resolve);
-				}));
-			}
+							inputAnimations.push(new Promise((resolve) => {
+								this.dataAnimationResolvers.set(data, resolve);
+							}));
+						}
+						return {
+							animatingData,
+							inputData,
+						};
+					},
+					// callback
+					() => Promise.all(inputAnimations).then( () => resolve() )
+				);
+			});
+			await inwardPromise;
 
-			await Promise.all(inputAnimations);
+			this.setState((prevState) => {
+				let {animatingData} = prevState;
+				for (const data of passEvent.in.values()) {
+					animatingData = animatingData.delete(data);
+				}
+				return {animatingData};
+			});
 
 			const outputAnimations = [];
+			const outwardPromise = new Promise( (resolve) => {
+				this.setState(
+					// updater
+					(prevState) => {
+						let {animatingData} = prevState;
 
-			for (const data of passEvent.in.values()) {
-				this.setState({
-					animatingData: this.state.animatingData.delete(data),
-				});
-			}
+						for (const [direction, data] of passEvent.out.entries()) {
+							animatingData = animatingData.set(data, {
+								direction,
+								isInward: false,
+								isErasing: this.props.status === 'stop',
+							});
+							outputAnimations.push(new Promise((resolve) => {
+								this.dataAnimationResolvers.set(data, resolve);
+							}));
+						}
+						return {animatingData};
+					},
+					// callback
+					() => Promise.all(outputAnimations).then( () => resolve() )
+				);
+			});
+			await outwardPromise;
 
-			for (const [direction, data] of passEvent.out.entries()) {
-				this.setState({
-					animatingData: this.state.animatingData.set(data, {
+			this.setState((prevState) => {
+				let {animatingData, outputData} = prevState;
+				for (const [direction, data] of passEvent.out.entries()) {
+					animatingData = animatingData.delete(data);
+					outputData = outputData.set(data, {
 						direction,
-						isInward: false,
 						isErasing: this.props.status === 'stop',
-					}),
-				});
-
-				outputAnimations.push(new Promise((resolve) => {
-					this.dataAnimationResolvers.set(data, resolve);
-				}));
-			}
-
-			await Promise.all(outputAnimations);
-
-			for (const [direction, data] of passEvent.out.entries()) {
-				this.setState({
-					animatingData: this.state.animatingData.delete(data),
-					outputData: this.state.outputData.set(data, {
-						direction,
-						isErasing: this.props.status === 'stop',
-					}),
-				});
-			}
+					});
+				}
+				return {animatingData, outputData};
+			});
 
 			this.props.onPassAnimationComplete(passEvent);
 		});
 
 		this.props.block.on('put', ({direction, data}) => {
 			if (this.props.boardEnds[direction]) {
-				let datum = this.state.outputData.get(data);
-				//assert(typeof datum !== 'undefined');
-				if (typeof datum === 'undefined') {
-					return;
-				}
+				this.setState( (prevState) => {
+					assert(prevState.outputData.has(data));
+					const datum = Object.assign(prevState.outputData.get(data), {isErasing: true});
 
-				datum.isErasing = true;
-
-				this.setState({
-					outputData: this.state.outputData.set(data, datum),
+					return {
+						outputData: prevState.outputData.set(data, datum),
+					};
 				});
 			} else {
-				this.setState({
-					outputData: this.state.outputData.delete(data),
-				});
+				this.setState( (prevState) => ({
+					outputData: prevState.outputData.delete(data),
+				}));
 			}
-		});
-
-		this.props.block.on('reject', (data) => {
-			let datum = this.state.inputData.get(data);
-			//assert(typeof datum !== 'undefined');
-			if (typeof datum === 'undefined') {
-				return;
-			}
-
-			datum.isErasing = true;
-
-			this.setState({
-				inputData: this.state.inputData.set(data, datum),
-			});
 		});
 	}
 
@@ -151,11 +176,11 @@ class BlockComponent extends React.Component {
 	handleStop() {
 		const eraseAll = (obj) => Object.assign(obj, {isErasing: true});
 
-		this.setState({
-			inputData: this.state.inputData.map(eraseAll),
-			animatingData: this.state.animatingData.map(eraseAll),
-			outputData: this.state.outputData.map(eraseAll),
-		});
+		this.setState( (prevState) => ({
+			inputData: prevState.inputData.map(eraseAll),
+			animatingData: prevState.animatingData.map(eraseAll),
+			outputData: prevState.outputData.map(eraseAll),
+		}));
 	}
 
 	handleDataAnimationComplete = (data) => {
@@ -165,11 +190,11 @@ class BlockComponent extends React.Component {
 	}
 
 	handleDataEraseAnimationComplete = (data) => {
-		this.setState({
-			inputData: this.state.inputData.delete(data),
-			animatingData: this.state.animatingData.delete(data),
-			outputData: this.state.outputData.delete(data),
-		});
+		this.setState( (prevState) => ({
+			inputData: prevState.inputData.delete(data),
+			animatingData: prevState.animatingData.delete(data),
+			outputData: prevState.outputData.delete(data),
+		}));
 	}
 
 	handleClick = (event) => (
